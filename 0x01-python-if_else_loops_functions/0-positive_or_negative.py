9947d21b15609679a53db101c53edc2ba443c51e
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-01 18:31:33 +0000

    The code was organized with the demands of pep8

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print ("{:d} {}".format(number, "is positive"))
+    print("{:d} {}".format(number, "is positive"))
 elif not number:
-    print ("{:d} {}".format(number, "is zero"))
-else :
-    print ("{:d} {}".format(number, "is negative"))
+    print("{:d} {}".format(number, "is zero"))
+else:
+    print("{:d} {}".format(number, "is negative"))
