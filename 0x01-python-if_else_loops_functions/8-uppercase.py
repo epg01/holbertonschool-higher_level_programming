2efe6c2226d451e0bfdef9038a270a6ae317926c
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-01 21:49:36 +0000

    It was organized with the demands of pep8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
     for Index in range(len(str)):
-        Character       = str[Index]
+        Character = str[Index]
         Solve_Character = Character
         if ord(Character) >= 97 and ord(Character) <= 122:
             Character = chr(ord(Character) - 97 + 65)
         if Index < (len(str) - 1):
-            print("{}".format(Character if ord(Character) >= 65 and \
+            print("{}".format(Character if ord(Character) >= 65 and
                               ord(Character) <= 90 else Character), end='')
         else:
-            print("{}".format(Character if ord(Character) >= 65 and \
+            print("{}".format(Character if ord(Character) >= 65 and
                               ord(Character) <= 90 else Character))
-
