479b576b691e1215fd2701e26154bd66fc71c6b1
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-01 22:13:53 +0000

    Exercise Finish

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for Number in range(0, 101):
+        if (not (Number % 3)) and (not (Number % 5)):
+            Message = 'FizzBuzz'
+        elif  (not (Number % 3)):
+            Message = 'Fizz'
+        elif (not (Number % 5)):
+            Message = 'Buzz'
+        else:
+            Message = ''
+        if Number != 100:
+            End = ' '
+        print("{}".format(Message if Message else Number), end=End)
