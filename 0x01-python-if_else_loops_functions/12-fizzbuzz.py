c4467913442e1371751131756643fa4994310014
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-01 22:19:10 +0000

    multiple spaces after keyword

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for Number in range(1, 101):
         if (not (Number % 3)) and (not (Number % 5)):
             Message = 'FizzBuzz'
-        elif  (not (Number % 3)):
+        elif (not (Number % 3)):
             Message = 'Fizz'
         elif (not (Number % 5)):
             Message = 'Buzz'
