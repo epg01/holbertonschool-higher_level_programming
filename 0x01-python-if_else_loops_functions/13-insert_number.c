9bc866444cd9090b49625b52e69ad8716093145e
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-01 23:33:52 +0000

    The negative number error was arranged

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -4,6 +4,16 @@ listint_t *insert_node(listint_t **head, int Number)
 {
 	if (!(*head))
 		return (NULL);
+	if (Number < 0 && (*head)->n > Number)
+	{
+		listint_t *New_Nodo;
+		New_Nodo = (listint_t *)malloc(sizeof(listint_t));
+		if (!(New_Nodo))
+			return (NULL);
+		New_Nodo->n    = Number;
+		New_Nodo->next = (*head);
+		return (New_Nodo);
+	}
 	else if (!((*head)->next) || ((*head)->next->n > Number))
 	{
 		listint_t *New_Nodo;
@@ -14,7 +24,6 @@ listint_t *insert_node(listint_t **head, int Number)
 			return (NULL);
 
 		New_Nodo->n    = Number;
-
 		New_Nodo->next = (*head)->next;
 		(*head)->next  = New_Nodo;
 		return (New_Nodo);
