67832bdee8670794553ee5b74802212a91a35572
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-01 19:59:17 +0000

    res the range of the range method

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for Number in range(0, 100):
-    print("{:d}{}{}".format(Number, " = ", hex(Number)))
+for Number in range(0, 99):
+    print("{:d} = {}".format(Number, hex(Number)))
 
