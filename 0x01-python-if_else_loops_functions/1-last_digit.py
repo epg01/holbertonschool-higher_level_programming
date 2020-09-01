c058fd30cb1d2f5bdb099add9197add6215204a9
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-01 19:00:35 +0000

    The way of evaluating sentences was organized

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 import random
 Number = random.randint(-10000, 10000)
-Last_Number = str(Number)[len(str(Number)) - 1]
-print("Last digit of ", Number, " is ", Last_Number, end=' ')
+if Number >= 0:
+    Last_Number = str(Number)[len(str(Number)) - 1]
+else:
+    Last_Number = "-"
+    Last_Number += str(Number)[len(str(Number)) - 1]
+
+print("Last digit of", Number, "is", Last_Number, end=' ')
+
 Last_Number = int(Last_Number)
-if Last_Number > 5:
-    print("{}".format("greater than 5"))
-elif Last_Number < 6 and not Number:
+
+if not Last_Number:
+    print("{}".format("and is 0"))
+elif Last_Number < 6:
     print("{}".format("less than 6 and not 0"))
 else:
-    print("{}".format("and is 0"))
+    print("{}".format("greater than 5"))
