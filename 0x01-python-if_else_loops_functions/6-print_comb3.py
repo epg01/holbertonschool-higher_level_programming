22c6aae0e2dbc22939e05dd6e79b6e3ea0f97de2
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-02 01:38:20 +0000

    Exercise Finish, try 4

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
-        if i < j:
-            if i != 8:
-                print("{}{}".format(i, j), end=", ")
-            else:
-                print("{}{}".format(i, j))
+    for j in range(i+1, 10):
+        print("{}{}".format(i, j), end=', '
+              if str(i) + str(j) != '89' else '\n')
