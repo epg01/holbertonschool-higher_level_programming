1daa038994ce393ebcecaa9e01ad054b8418a8ce
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-03 20:24:22 +0000

    The exercise was done recursively

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,32 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * check_cycle - check for a cycle lined list
- * @list: pointer to head node
- *
- * Return: integer
+ * Form_Recursive - Function that checks if it is a loop or not recursively.
+ * @Element: an element of the data structure.
+ * @Head: Pointer always in the lead.
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
+
+int Form_Recursive(listint_t *Element, listint_t *Head)
+{
+	if (!(Element))
+		return (0);
+	else if (Element->next == Head)
+		return (1);
+	else
+		return (Form_Recursive(Element->next, Head));
+}
+
+
+/**
+ * check_cycle - check for a cycle lined list.
+ * @head: Pointer data structure.
+ * Return: integer.
+ */
+
 int check_cycle(listint_t *head)
 {
-	listint_t *hare = head;
-	listint_t *turtle = head;
-
-	while (head && hare->next && hare->next->next)
-	{
-		hare = hare->next->next;
-		turtle = turtle->next;
-		if (turtle == hare)
-			return (1);
-	}
-	return (0);
+	return (Form_Recursive(head, head));
 }
