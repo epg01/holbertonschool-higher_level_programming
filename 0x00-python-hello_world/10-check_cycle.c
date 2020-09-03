60d7f8a689415c07cb3859b5cd2d518f43252305
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-03 22:17:27 +0000

    3143

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,24 +9,18 @@
  * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
 
-int Form_Recursive(listint_t *Tortuga, listint_t *Conejto)
+int recursiva(listint_t *Tortuga, listint_t *Conejto)
 {
+  
         if (!Tortuga || !Conejto) // Element == NULL
 		return (0);
         else if (Tortuga == Conejto)
 		return (1);
         else
-                return (Form_Recursive(Tortuga->next, Conejto->next->next));
+                return (recursiva(Tortuga->next, Conejto->next->next));
 }
-
-
-/**
- * check_cycle - check for a cycle lined list.
- * @head: Pointer data structure.
- * Return: integer.
- */
-
 int check_cycle(listint_t *head)
 {
-	return (Form_Recursive(head, head->next->next));
+	recursiva(head, head->next->next);
+  
 }
