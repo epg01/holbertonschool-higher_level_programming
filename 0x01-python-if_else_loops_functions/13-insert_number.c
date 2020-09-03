95dbb616b8658e6a4e7d9117799d550ddcea20f5
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-03 11:39:02 +0000

    Recursion is applied

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,54 +1,30 @@
 #include "lists.h"
+
 /**
  * insert_node - prints all elements of a listint_t list
  * @head: pointer to head of list
- * @number: the number
+ * @Number: the number
  * Return: the new node
  */
-listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *new;
-	listint_t *aux;
-	int i = 0;
 
-	if (head == NULL)
+listint_t *insert_node(listint_t **head, int Number)
+{
+	if (!(*head))
 		return (NULL);
-	aux = *head;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-	new->next = NULL;
-	if (*head == NULL)
+	else if (!((*head)->next) || ((*head)->next->n > Number))
 	{
-		*head = new; 
+		listint_t *New_Nodo;
+
+		New_Nodo = (listint_t *)malloc(sizeof(listint_t));
+		if (!(New_Nodo))
+			return (NULL);
+		New_Nodo->n    = Number;
+		if ((*head)->n > Number)
+			New_Nodo->next = *head, (*head) = New_Nodo;
+		else
+			New_Nodo->next = (*head)->next, (*head)->next  = New_Nodo;
+		return (New_Nodo);
 	}
 	else
-	{
-		if (number < aux->n)
-		{
-			new->next = aux;
-			*head = new;
-			i = 1;
-		}
-		else
-		{
-			while (aux->next != NULL)
-			{
-				if (number < aux->next->n)
-				{
-					new->next = aux->next;
-					aux->next = new;
-					break;
-				}
-				aux = aux->next;
-			}
-		}
-		if (aux->next == NULL && i == 0)
-		{
-			aux->next = new;
-			new->next = NULL;
-		}
-	}
-	return (new);
+		return (insert_node(&(*head)->next, Number));
 }
