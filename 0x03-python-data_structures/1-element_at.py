abf1e93efdec0b9e31467abe23db5456627f6dea
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-08 23:37:24 +0000

    Return none was added in the last line and it was organized with pep8

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
     if idx < len(my_list):
         if not idx:
@@ -7,3 +6,4 @@ def element_at(my_list, idx):
         else:
             idx -= 1
             return element_at(my_list[1:], idx)
+    return None
