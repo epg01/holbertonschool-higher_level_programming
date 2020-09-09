a733a80cb101b54b4c7f03ad872fe187947a626a
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-09 03:28:56 +0000

    Hotfix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,11 @@
-#!/bin/python3
-def P_print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for idx in range(len(matrix[0])):
-            if idx != (len(matrix[0]) - 1):
-                print("{:d}".format(matrix[0][idx]), end=' ')
-            else:
-                print("{:d}".format(matrix[0][idx]))
-        return P_print_matrix_integer(matrix[1:])
+#!/usr/bin/python3
 
 
 def print_matrix_integer(matrix=[[]]):
-    if [] not in matrix:
-        return (P_print_matrix_integer(matrix))
-    else:
+    for i in matrix:
+        for j in i:
+            if j == i[-1]:
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end=" ")
         print()
