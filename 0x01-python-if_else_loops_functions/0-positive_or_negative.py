2487309ce7ec3c66088d25466235cc1241505e90
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-24 21:40:50 +0000

    The format of the str.format() method was changed

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import random
 Number = random.randint(-10, 10)
-Message = ""
 if Number is 0:
     Message = "is zero"
 elif Number > 0:
     Message = "is positive"
 else:
     Message = "is negative"
-print("{Number:d} {Message:s}".format(Number=Number, Message=Message))
+print("{:d} {:s}".format(Number, Message))
