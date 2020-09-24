1a59d0b614720a44386687dcf4e1b637a23b383f
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-24 23:57:43 +0000

    the Namber variable was added for namber

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
-Number = random.randint(-10, 10)
-if Number is 0:
-    print("{0:d} is zero".format(Number))
-elif Number > 0:
-    print("{0:d} is positive".format(Number))
+number = random.randint(-10, 10)
+if number is 0:
+    print("{} is zero".format(number))
+elif number > 0:
+    print("{} is positive".format(number))
 else:
-    print("{0:d} is negative".format(Number))
+    print("{} is negative".format(number))
