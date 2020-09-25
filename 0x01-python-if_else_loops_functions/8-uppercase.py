f5c5f4af2ed77fe0d3291cb57ff3300ade7e3138
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-25 01:22:59 +0000

    Only two print statements remained

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def uppercase(String):
-
+    Separator = ''
+    Counter_Length_String = 1
     for Character in String:
         Number = ord(Character)
+        if Counter_Length_String == len(String):
+            Separator = '\n'
+
         if Number >= 97 and Number <= 122:
-            print("{0:s}".format(chr(Number - 97 + 65)), end='')
+            print("{0:s}".format(chr(Number - 97 + 65)), end=Separator)
         else:
-            print("{0:s}".format(Character), end='')
-    print()
+            print("{0:s}".format(Character), end=Separator)
+        Counter_Length_String += 1
