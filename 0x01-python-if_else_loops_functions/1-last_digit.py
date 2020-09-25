fbc0e837bcb942ba0ef169025041d0cf7cc14c35
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-25 00:11:07 +0000

    More pieceholder was added

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
+Initial_M = "Last digit of"
 if number < 0:
     Last_Digit = (number * (-1)) % 10
     Last_Digit *= (-1)
@@ -9,10 +10,11 @@ else:
     Last_Digit = number % 10
 
 if Last_Digit is 0:
-    print("Last digit of {} is {} and is 0".format(number, Last_Digit))
+    Message = "is zero"
+    print("{} {} is {} {}".format(Initial_M, number, Last_Digit, Message))
 elif Last_Digit > 5:
     Message = "and is greater that 5"
-    print("Last digit of {} is {} {}".format(number, Last_Digit, Message))
+    print("{} {} is {} {}".format(Initial_M, number, Last_Digit, Message))
 else:
     Message = "and is less that 6 and not 0"
-    print("Last digit of {} is {} {}".format(number, Last_Digit, Message))
+    print("{} {} is {} {}".format(Initial_M, number, Last_Digit, Message))
