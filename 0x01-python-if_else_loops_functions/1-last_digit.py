ed2e055064d1e11fb0dffcf28de77024e70ad085
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-25 00:00:57 +0000

    The Namber variable was changed for namber

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
-Number = random.randint(-10000, 10000)
+number = random.randint(-10000, 10000)
 
 Message_Initial = "Last digit of"
-if Number < 0:
-    Last_Digit = (Number * (-1)) % 10
+if number < 0:
+    Last_Digit = (number * (-1)) % 10
     Last_Digit *= (-1)
 else:
-    Last_Digit = Number % 10
+    Last_Digit = number % 10
 
 if Last_Digit is 0:
     Message = "and is 0"
@@ -16,5 +16,5 @@ elif Last_Digit > 5:
 else:
     Message = "and is less that 6 and not 0"
 
-print("{:s} {Number:d} ".format(Message_Initial, Number=Number), end='')
+print("{:s} {Number:d} ".format(Message_Initial, Number=number), end='')
 print("is {:d} {Message:s}".format(Last_Digit, Message=Message))
