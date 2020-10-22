ca2672d914bcdad8ace57431dca5e006e23a73ae
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-22 23:13:21 +0000

    #0.4 The file was completely changed

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_list_integer(my_list=[]):
-    if my_list != []:
-        for value in my_list:
-            print("{0:d}".format(value))
+    for value in my_list:
+        print("{:d}".format(value))
