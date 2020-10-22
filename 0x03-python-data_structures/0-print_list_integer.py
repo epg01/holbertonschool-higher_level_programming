9a9bc81db604b2b7a3b3f0ae392b88943eef657b
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-22 23:02:18 +0000

    #0.1 The first condition if was deleted

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-if __name__ != "__main__":
-
-    def print_list_integer(my_list=[]):
-
-        if my_list != []:
-            for value in my_list:
-                print("{0}".format(value))
+def print_list_integer(my_list=[]):
+    if my_list != []:
+        for value in my_list:
+            print("{0}".format(value))
