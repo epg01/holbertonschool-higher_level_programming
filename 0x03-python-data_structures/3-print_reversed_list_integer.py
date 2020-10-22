7609b8f0c091d58edfc53ef9dc136bcf6b84d0ce
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-22 23:41:06 +0000

    #5.1 The first condition wa delete

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == []:
-        print()
-    else:
-        my_list_copy = my_list[::-1]
-        for value in my_list_copy:
-            print(value)
+    my_list_copy = my_list[::-1]
+    for value in my_list_copy:
+        print(value)
