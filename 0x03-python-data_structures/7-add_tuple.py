6bda33081fd2bcef25f75b39d0a456e25fa50e05
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-23 13:03:52 +0000

    #7.2 A Handle error was changed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -16,4 +16,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         elif LenA >= 2 and LenB < 2:
             return tuple_a[0] + tuple_b[0], tuple_a[1]
         else:
-            return tuple_a[0] + tuple_b[0], 0
+            if not LenA and not LenB:
+                return 0, 0
+            else:
+                return tuple_a[0] + tuple_b[0], 0
