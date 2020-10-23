f5b5f07a83c7d37d4f5c2e4fc33a23bd5ed6e702
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-23 00:05:53 +0000

    #6 Function that replaces an element in a list at a specific position without modifying the original list (like in C)

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is not None and idx in range(len(my_list)):
+        my_list_clone = list(my_list)
+        my_list_clone[idx] = element
+        return my_list_clone
