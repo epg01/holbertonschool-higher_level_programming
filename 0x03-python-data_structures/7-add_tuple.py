ac360d5bd50009136e90967c25fa56632cd3a7dd
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-23 13:00:51 +0000

    #7.1 The condition of the one only value of the one tuple was add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    LenA = len(tuple_a)
-    LenB = len(tuple_b)
-    if not LenA and LenB >= 2:
-        return tuple_b
-    elif not LenB and LenA >= 2:
-        return tuple_a
-    elif LenA >= 2 and LenB >=2:
-        return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    elif LenA < 2 and LenB >= 2:
-        return tuple_a[0] + tuple_b[0], tuple_b[1]
-    elif LenA >= 2 and LenB < 2:
-        return tuple_a[0] + tuple_b[0], tuple_a[1]
+    if tuple_a is None or tuple_b is None:
+        pass
+    else:
+        LenA = len(tuple_a)
+        LenB = len(tuple_b)
+        if not LenA and LenB >= 2:
+            return tuple_b
+        elif not LenB and LenA >= 2:
+            return tuple_a
+        elif LenA >= 2 and LenB >=2:
+            return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+        elif LenA < 2 and LenB >= 2:
+            return tuple_a[0] + tuple_b[0], tuple_b[1]
+        elif LenA >= 2 and LenB < 2:
+            return tuple_a[0] + tuple_b[0], tuple_a[1]
+        else:
+            return tuple_a[0] + tuple_b[0], 0
