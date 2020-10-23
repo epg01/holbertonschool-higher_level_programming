93fc428b864c4f5a07985550c8feda588b263084
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-23 13:02:04 +0000

    #7.2 pep8 was organized

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,7 +9,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
             return tuple_b
         elif not LenB and LenA >= 2:
             return tuple_a
-        elif LenA >= 2 and LenB >=2:
+        elif LenA >= 2 and LenB >= 2:
             return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
         elif LenA < 2 and LenB >= 2:
             return tuple_a[0] + tuple_b[0], tuple_b[1]
