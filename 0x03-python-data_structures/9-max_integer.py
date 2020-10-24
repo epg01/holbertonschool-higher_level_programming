c5f1ac0ac2bb1595640f106013181ceb76891668
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-24 16:52:50 +0000

    A built-in python function is implemented to sort

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-def search_max(my_list, max_value, length):
-    if not length:
-        if max_value < my_list[0]:
-            return my_list[0]
-        else:
-            return max_value
-    else:
-        if max_value < my_list[0]:
-            max_value = my_list[0]
-        return search_max(my_list[1:], max_value, length - 1)
-
-
 def max_integer(my_list=[]):
     if my_list is not None and my_list:
-        return (search_max(my_list, my_list[0], len(my_list) - 1))
+        my_list.sort()
+        return my_list[-1]
