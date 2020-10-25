05c8bc9f96272131b438e22fe4d03fa103d75fc6
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-25 12:23:18 +0000

    added a compressed list

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    number = list(map(lambda Number: Number, my_list))
+    number = [number for item in my_list]
     return list(map(lambda x, number: x * number, my_list, number))
