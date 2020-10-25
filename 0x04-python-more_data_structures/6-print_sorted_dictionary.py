2b24905df8f2b6edcff010d843e84d004d044798
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-25 01:30:45 +0000

    Added the colon

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 def print_sorted_dictionary(a_dictionary):
     list_orden = sorted(list(zip(a_dictionary.items())))
     for item in list_orden:
-        print("{0:s} {1:s}".format(str(item[0][0]), str(item[0][1])))
+        print("{0:s}: {1:s}".format(str(item[0][0]), str(item[0][1])))
