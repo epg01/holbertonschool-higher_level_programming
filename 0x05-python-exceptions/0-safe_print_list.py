ab50e446889c99161df410fe2e8e3dc3acc7e2dd
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-28 20:02:27 +0000

    if condition was removed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list != []:
-        try:
-            number_of_element_print = int()
-            for item_print in range(x):
-                print("{0}".format(item_print), end='')
-                number_of_element_print += 1
-            print()
-        except:
-            return number_of_element_print
+    try:
+        number_of_element_print = int()
+        for item_print in range(x):
+            print("{0}".format(item_print), end='')
+            number_of_element_print += 1
+        print()
+    except:
         return number_of_element_print
+    return number_of_element_print
