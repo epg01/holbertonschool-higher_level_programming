d5ad9a07f6360845daa3428eed007574037608a5
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-10-29 20:10:58 +0000

    A tuple of exceptions was placed and the try was inserted inside the for

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,12 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        Number_print = int()
-        for index in range(x):
-            hold_item = my_list[index]
-            if type(int()) is type(hold_item):
-                print("{0:d}".format(hold_item), end='')
-                Number_print += 1
-        print()
-    except:
-        raise
-
+    Number_print = int()
+    for index in range(x):
+        try:
+            print("{0:d}".format(my_list[index]), end='')
+            Number_print += 1
+        except (ValueError, TypeError):
+            pass
+    print()
     return Number_print
