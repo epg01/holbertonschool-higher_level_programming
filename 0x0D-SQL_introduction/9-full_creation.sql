aabf8f34d45976cf624ea409b7299d5d63f29a1d
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-12-13 10:53:40 -0500

    Version 1

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,9 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table(
        name VARCHAR(256),
        score INT
 );
-
 INSERT INTO second_table (id, name, score) VALUES
        (1, "John", 10),
-       (1, "Alex", 3),
+       (2, "Alex", 3),
        (3, "Bob", 3),
        (4, "George", 8);
