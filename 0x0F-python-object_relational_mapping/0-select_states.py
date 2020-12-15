a4aa60a82af82dabfe185f840711e4320dd5f08c
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-12-15 05:14:05 -0500

    script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+!#/usr/bin/python3
+import sys
+import MySQLdb
+
+"""
+   Script that lists all states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+
+    usrn = sys.argv[1]
+    passw = sys.argv[2]
+    db_name = sys.argv[3]
+    hos_t = "localhost"
+
+    db = MySQLdb.connect(host=hos_t, user=usrn,  passwd=passw, db=db_name)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
