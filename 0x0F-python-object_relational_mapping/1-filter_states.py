c22990e5c33c387839d98bc3b4ef9aab5768755b
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-12-16 11:05:33 -0500

    Hotfix

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+   Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+
+
+if __name__=='__main__':
+    import sys
+    import MySQLdb
+
+    DB_HOST = 'localhost'
+    DB_PORT = 3306
+    DB_USER = sys.argv[1]
+    DB_PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    CHARSET = 'utf8'
+
+    # Nos conectamos a la base de datos.
+
+    conn = MySQLdb.connect(host=DB_HOST,
+                           port=DB_PORT,
+                           user=DB_USER,
+                           passwd=DB_PASS,
+                           db=DB_NAME,
+                           charset=CHARSET)
+
+    # Colocamos el cursor.
+
+    cursor = conn.cursor()
+
+    # Ejecutamos el comando
+
+    cursor.execute("""SELECT * FROM states WHERE (name LIKE BINARY 'N%')\
+    ORDER BY states.id ASC""")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    conn.close()
