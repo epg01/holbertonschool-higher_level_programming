b3d959ff14769c1102047aff13acf3442adfa48f
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-12-16 11:06:47 -0500

    Hotfix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-   Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+   Script that lists all states with a name starting with N (upper N) from the
+   database hbtn_0e_0_usa
 """
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     import sys
     import MySQLdb
 
