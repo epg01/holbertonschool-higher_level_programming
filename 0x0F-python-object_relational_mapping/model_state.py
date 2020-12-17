0dc24324761797afd885cfd76530fba0bbee6142
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-12-17 09:54:57 -0500

    Hotfix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,26 +6,25 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
-# La clase state hereda de la instancia Base
+Base = declarative_base()
 
 
 class State(Base):
     """
     Args:
-         Base(Objeto): Instancia
+        Base ([type]): [description]
     """
-
     # Enlaces a los estados de la tabla States.
 
     __tablename__ = 'states'
+
     # id de atributo de clase que representa una columna de un entero
     # único generado automáticamente, no puede ser nulo y es una clave principa
 
-    id = sqlalchemy.Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
     # Atributo de clase (name) que representa una columna de una
     # cadena con un máximo de 128 caracteres y no puede ser nulo
 
-    name = sqlalchemy.Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
