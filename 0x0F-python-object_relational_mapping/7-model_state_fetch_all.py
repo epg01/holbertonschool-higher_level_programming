6c5f79cbfaeeda5fa92b06a5fab9845e1211f3d7
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-12-17 10:26:14 -0500

    Hotfix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa
 """
-   Script that lists all State objects from the database hbtn_0e_6_usa.
-"""
+import sys
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-import sys
 
 
 Base = declarative_base()
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # Nos conectamos a la base de datos.
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True))
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     # llamamos al metodo MetaData.create_all(), pasando nuestro "engine"
     # como fuente de conectividad de base de datos.
@@ -26,8 +23,12 @@ if __name__ == '__main__':
 
     # Servirá como fabrica para nuevos objetos session.
 
-    Session = sessionmaker(bind=engine)  # bind=engine una vez que el motor
+    Session = sessionmaker()
+
+    # bind=engine una vez que el motor
     # este disponible.
+
+    Session.configure(bind=engine)
 
     # siempre que necesite tener una conversación con la base de datos,
     # cree una instancia de Session
