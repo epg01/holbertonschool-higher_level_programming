94b7ada7b8a7eaa8e3ecd1c1efd4ed1ec2eaa67c
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 21:42:39 +0000

    The list function was changed to len

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or (idx not in range(list(my_list))):
+    if idx < 0 or (idx not in range(len(my_list))):
         pass
     else:
         my_list[idx] = element
