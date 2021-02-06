1b30c14bf67b61fa32e3413ec051ee59714d81be
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 21:56:46 +0000

    A shallow copy was created

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for item in my_list:
+    Copy_List_Superficial = list(my_list)
+    Copy_List_Superficial.reverse()
+    for item in Copy_List_Superficial:
         print('{Number:d}'.format(Number=item))
