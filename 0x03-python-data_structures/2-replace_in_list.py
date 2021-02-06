1e883672c99657b425f9d27f8f94cbe778912b1e
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 21:37:27 +0000

    Third exercise done

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 and (idx not in range(list(my_list))):
+        pass
+    else:
+        my_list[idx] = element
+    return my_list
