7fc52ba82c555858ea1b046d60cf6febb06a7c03
Emmanuel Palacio <emmanuelpalacio1@hotmail.com>
2021-02-06 18:51:36 +0000

    Fourth finished example

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+if __name__ == '__main__':
+    import sys
+
+    # Convert the str type to int type.
+
+    try:
+
+        List_Num = [int(item) for item in sys.argv[1:]]
+
+    except:
+
+        print('0')
+
+    # Then Convert the list an iterable data type --> iter()
+
+    Iter_Num = iter(List_Num)
+    print("{result_sum}".format(result_sum=sum(Iter_Num)))
