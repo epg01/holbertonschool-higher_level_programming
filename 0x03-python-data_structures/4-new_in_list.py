7466ba9ffad033b7f97f26e6092b299f551e9690
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 22:01:01 +0000

    Fifth exercise done

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or (idx not in range(len(my_list))):
+        pass
+    else:
+        copy_list_S = list(my_list)
+        copy_list_S[idx] = element
+    return copy_list_S
