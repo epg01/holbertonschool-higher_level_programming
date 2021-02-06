a151b53bee8db04baaaf948b6b06c7b0ccf8e3c0
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 22:30:02 +0000

    Code organized

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    copy_list_S = my_list.copy()
     if idx < 0 or (idx not in range(len(my_list))):
         pass
     else:
-        copy_list_S = my_list.copy()
         copy_list_S[idx] = element
     return copy_list_S
