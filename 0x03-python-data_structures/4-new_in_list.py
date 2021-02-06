1cb75803418424dd5504e53bcb1add3a370d8fd2
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 22:27:28 +0000

    The copy method was implemented

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or (idx not in range(len(my_list))):
         pass
     else:
-        copy_list_S = list(my_list)
+        copy_list_S = my_list.copy()
         copy_list_S[idx] = element
     return copy_list_S
