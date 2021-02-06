44d792291f730bf72886ad9ea3339d3ce501512a
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 21:40:30 +0000

    The and by or was changed

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 and (idx not in range(list(my_list))):
+    if idx < 0 or (idx not in range(list(my_list))):
         pass
     else:
         my_list[idx] = element
