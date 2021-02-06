9ea2ac7989d626e5b2be20b377840c0459b40e3e
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 21:13:42 +0000

    The code was redone from scratch

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    try:
-        for item in my_list:
-            print("{Number}".format(Number=item))
-    except:
-        pass
+    for i in my_list:
+        print("{:d}".format(i))
