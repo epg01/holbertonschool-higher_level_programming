5ccd116027b36c5bad23e36a7bcf0cbc7fee6866
Emmanuel Palacio <emmanuelpalacio1@hotmail.com>
2021-02-06 18:34:37 +0000

    The output was staged, specifically the output of various arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         print("{length} argument:".format(length=1))
         print("1: {name_argv}".format(name_argv=sys.argv[1]))
     elif (len(sys.argv)) > 1:
-        print("{length} arguments:".format(length=len(sys.argv)))
+        print("{length} arguments:".format(length=len(sys.argv[1:])))
         for num_argv, argv in enumerate(sys.argv[1:], start=1):
             print("{num}: {argv}".format(num=num_argv, argv=argv))
     else:
