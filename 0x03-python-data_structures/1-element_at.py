1e5aed1370ed46c7fa87daaf4ddacc1bedb9cfc3
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 21:20:38 +0000

    The if condition was organized

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 def element_at(my_list, idx):
-    try:
-        if idx < 0:
-            return None
-        elif my_list[idx]:
-            return my_list[idx]
-    except IndexError:
-            return None
+    if (idx >= 0) and (idx in set(range(len(my_list)))):
+        return my_list[idx]
+    else:
+        return None
