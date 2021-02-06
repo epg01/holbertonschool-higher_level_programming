e22143c2da0126da5ac7f13544ee1810a3834eef
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-06 21:29:51 +0000

    Control flow organized

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 def element_at(my_list, idx):
-    if (idx >= 0) and (idx in set(range(len(my_list)))):
+    if idx < 0:
+        return None
+    elif idx in range(len(my_list)):
         return my_list[idx]
     else:
-        return None
+        None
