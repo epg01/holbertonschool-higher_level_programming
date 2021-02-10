c2554583f0fc5c879d6cd9f301d79e2a0a72bac5
epg01 <60383826+epg01@users.noreply.github.com>
2021-02-09 22:22:54 -0500

    Hotfix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-const args = (process.argv);
 const request = require('request');
-const url = args[2];
-const target = 'https://swapi-api.hbtn.io/api/people/18/';
-const wedgeList = [];
-const retList = [];
-request(url, function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
-    results.map(x =>
-      wedgeList.push(x.characters));
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-  wedgeList.map(x => x.includes(target) ? retList.push('stay on target') : {});
-  console.log(retList.length);
 });
