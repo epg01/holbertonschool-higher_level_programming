be20ffd56349fe975a97cbec9d58a19b07d577aa
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-12 22:39:05 +0000

    Hotfix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -8,5 +8,9 @@ function Recursive (Number, Factorial) {
 }
 
 if ((isFinite(process.argv[2]) && (Number(process.argv[2]) >= 0)) || (isNaN(process.argv[2]))) {
+  if (1/Number(process.argv[2]) === -Infinity || 1/Number(process.argv[2]) === Infinity) {
+    console.log(1);
+    return ;
+  }
   console.log(Recursive(process.argv[2], Number(1)));
 }
