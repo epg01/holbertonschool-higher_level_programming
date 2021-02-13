afeaa8eab7bf016faa16657efa7c4452294ef0de
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-13 19:41:37 +0000

    Hotfix

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = Number(this.width);
+    this.width = Number(this.height);
+    this.height = Number(temp);
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
