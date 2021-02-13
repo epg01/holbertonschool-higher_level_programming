c850006f6d359620a6ff045edbe9411f90c7e3e8
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-13 20:13:24 +0000

    Hotfix

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const _Square = require('./5-square');
+
+class Square extends _Square {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
