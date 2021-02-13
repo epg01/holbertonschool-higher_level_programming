3f3aabbb028f66808d64b21f4fc278541d85bbc4
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-02-13 21:31:55 +0000

    Hotfix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-
 exports.converter = function (base) {
-    function Conversor(string) {
-	return (parseInt(String(string), base));
-    }
-    return (Conversor);
+  function Conversor (string) {
+    return (string.toString(base));
+  }
+  return (Conversor);
 };
