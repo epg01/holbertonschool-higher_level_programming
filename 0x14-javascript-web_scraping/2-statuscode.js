ba54fed0e6232fba72d19bdaa4125a0b3f263876
Emmanuel Palacio <emmanuelpalacio1@hotmail.com>
2021-02-14 14:34:50 +0000

    Hotfix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) return console.error('erro', error);
-  console.log('code', response && response.statusCode); // Print the response status code if a response was received
+  console.log('code:', response && response.statusCode); // Print the response status code if a response was received
 });
